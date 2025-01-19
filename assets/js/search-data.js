9a087126145952e3fb398898392281bf4089d4cf
DrDang <ChristopherDang1992@yahoo.com>
2025-01-19 20:16:41 +0000

    Deploying to gh-pages from @ DrDang/DrDang.github.io@d9cd27864ef500c5a07ce563184c96b2ad4b0b73 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "A growing collection of my cool projects.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
@@ -400,51 +400,31 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-autonomous-adcp2-firmware-software-load-amp-audit-system",
+          title: 'Autonomous ADCP2 Firmware/Software Load &amp;amp; Audit System',
+          description: "",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+              window.location.href = "/projects/1_PD52002542/";
+            },},{id: "projects-visual-management-system-for-test-equipment",
+          title: 'Visual Management System for Test Equipment',
+          description: "How I enhanced operational efficiency through 6-Sigma Visual Management",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+              window.location.href = "/projects/2_newVisualManagement/";
+            },},{id: "projects-modernizing-1553-vxi-card-integration-for-c-5",
+          title: 'Modernizing 1553 VXI Card Integration for C-5',
+          description: "I enabled seamless migration from a Condor to Abaco 1553 VXI card without disrupting existing test infrastructure.",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+              window.location.href = "/projects/3_1553-Port-for-C-5/";
+            },},{id: "projects-a-better-automated-rcca",
+          title: 'A Better, Automated RCCA',
+          description: "An RCCA software tool to help us solve problems!",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
+              window.location.href = "/projects/5_RCCA-Tool/";
+            },},{id: "projects-rcca-varying-recorded-on-times",
+          title: 'RCCA - Varying Recorded On Times',
+          description: "",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
+              window.location.href = "/projects/7_RCCA_varyingRecordOnTimes/";
             },},{
         id: 'social-email',
         title: 'email',
